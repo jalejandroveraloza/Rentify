d5d7e9e801a755ee8478d045be5be9fbf24bea5e
Andre <andrepaesbm@gmail.com>
2023-06-07 20:14:53 -0600

    created ProductList and RentForm components, installed Tailwind, refactored App.js

--- frontend_rentify/src/App.test.js
+++ frontend_rentify/src/App.test.js
@@ -1,8 +1,17 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import ProductList from './components/ProductList';
+import RentForm from './components/RentForm';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+const App = () => {
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/" component={ProductList} />
+        <Route exact path="/rent/:productId" component={RentForm} />
+      </Switch>
+    </Router>
+  );
+};
+
+export default App;
