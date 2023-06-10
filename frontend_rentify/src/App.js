dc99863df2027413d3a16502dc834cfc4ef47691
Andre <andrepaesbm@gmail.com>
2023-06-09 20:35:53 -0600

    Merge conflicts resolved

--- frontend_rentify/src/App.js
+++ frontend_rentify/src/App.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React, { useEffect, useState } from 'react';
 
 function App() {
@@ -29,26 +28,7 @@ function App() {
         {data.map(item => (
           <li key={item.id}>{item.name}</li>
         ))}
-      </ul>
-=======
-// src/App.js
-import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import ProductList from './components/ProductList';
-import RentForm from './components/RentForm';
-
-const App = () => {
-  return (
-    <div className="bg-gray-100 min-h-screen">
-      <Router>
-        <div className="container mx-auto py-8">
-          <Switch>
-            <Route exact path="/" component={ProductList} />
-            <Route exact path="/rent/:productId" component={RentForm} />
-          </Switch>
-        </div>
-      </Router>
->>>>>>> d5d7e9e801a755ee8478d045be5be9fbf24bea5e
+      </ul>    
     </div>
   );
 };
