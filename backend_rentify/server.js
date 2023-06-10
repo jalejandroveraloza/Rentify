23d6bda8fa8cbae5cf82bdfd7e69f68f817d74ed
Jorge Alejandro Vera Loza <vejo17@gmail.com>
2023-06-09 19:23:42 -0700

    trying to connect backend and frontend

--- backend_rentify/server.js
+++ backend_rentify/server.js
@@ -1,12 +1,39 @@
 const Pool = require("pg").Pool
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const router = express.Router();
+
+//middleware
+
+app.use(cors());
+app.use(express.json());
 
 
 const pool = new Pool({
-  user: "postgres",
-  password: "12345",
+  user: "labber",
+  password: "labber",
   host: "localhost",
-  port: 5000,
+  port: 5432,
   database: "rentify"
 });
+
+app.get('/api/users', (req, res) => {
+  // Query the database to fetch data
+  pool.query('SELECT * FROM customers', (err, result) => {
+    if (err) {
+      console.error('Error executing query', err);
+      res.status(500).json({ error: 'An error occurred' });
+    } else {
+      // Send the fetched data as a response
+      res.json(result.rows);
+    }
+  });
+})
+
+app.listen(5000, () => {
+  console.log("server has started on port 5000");
+});
+
 
 module.exports = pool;
--- frontend_rentify/src/App.js
+++ frontend_rentify/src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
 
 function App() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    // Fetch data from the server when the component mounts
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    try {
+      // Make an HTTP GET request to the server's API endpoint
+      const response = await fetch('/api/users');
+      const responseData = await response.json();
+
+      // Update the data state with the fetched data
+      setData(responseData);
+    } catch (error) {
+      console.error('Error fetching data', error);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Test1</h1>
+      <ul>
+        {data.map(item => (
+          <li key={item.id}>{item.name}</li>
+        ))}
+      </ul>
     </div>
   );
 }
