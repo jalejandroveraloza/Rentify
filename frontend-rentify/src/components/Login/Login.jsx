5d6913524eec3f3d7fccf8c8d54850644993fb98
Andre <andrepaesbm@gmail.com>
2023-06-14 20:49:30 -0600

    Refactor and give some styling to the login page and also refactor the login component

--- frontend-rentify/src/components/Login/Login.jsx
+++ frontend-rentify/src/components/Login/Login.jsx
@@ -3,34 +3,57 @@ import { FcGoogle } from 'react-icons/fc';
 import { GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
 import { auth } from '../../utils/firebase';
 import { useAuthState } from 'react-firebase-hooks/auth';
-//import { useHistory } from 'react-router-dom';
 import "./login.css";
 
-
 const Login = () => {
-
   const [user, loading] = useAuthState(auth);
-  //const history = useHistory(); // Get the history object
 
-  //sign in with google
   const googleProvider = new GoogleAuthProvider();
-  const GoogleLogin = async () => {
+
+  const handleGoogleLogin = async () => {
     try {
       const result = await signInWithPopup(auth, googleProvider);
       console.log(result.user);
-      //history.push("/"); // Redirect to the "/" route after successful sign-in
-
     } catch (error) {
       console.log(error);
     }
-  }
+  };
+
+  const handleEmailLogin = () => {
+    // Handle email login logic here
+  };
+
+  const handleRegister = () => {
+    // Handle registration logic here
+  };
+
   return (
-    
     <div className="login-container">
       <div className="login-box">
-      <h2>Join Today</h2>
-        <h3>Sign in with your Google account</h3>
-        <button onClick={GoogleLogin} className="google-button"><FcGoogle /> Sign in with Google</button>
+        <h2 className="join-today">Join Today</h2>
+        <div className="login-options">
+          <h3>Sign in using:</h3>
+          <button onClick={handleEmailLogin} className="email-button">
+            Email
+          </button>
+        </div>
+        <div className="login-options">
+          <h3>Don't have an account? Sign up:</h3>
+          <button onClick={handleRegister} className="register-button">
+            Register
+          </button>
+        </div>
+        <div className="divider">
+          <hr />
+          <p>Or</p>
+          <hr />
+        </div>
+        <div className="login-options">
+          <h3>Sign in with your Google account</h3>
+          <button onClick={handleGoogleLogin} className="google-button">
+            <FcGoogle /> Sign in with Google
+          </button>
+        </div>
       </div>
     </div>
   );
